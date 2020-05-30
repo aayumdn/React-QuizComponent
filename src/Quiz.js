b64feab6af36fe2cf8ec645fc65defe0312c17ff
aayumdn <46893263+aayumdn@users.noreply.github.com>
2020-05-30 01:41:00 +0100

    final

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,11 +15,15 @@ class Quiz extends Component {
         this.setState({quiz_position : this.state.quiz_position + 1})
     }
 
+    handleResetClick(){
+        this.setState({quiz_position : 1})
+    }
+
     render(){
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length ? true : false
         if(isQuizEnd){
             return(
-                <QuizEnd />
+                <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/>
             )
         } else{
             return(
